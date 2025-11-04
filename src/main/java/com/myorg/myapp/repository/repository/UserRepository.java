6ebdfc50e612a183eaceb965fd685e5bfadfbee3
pedaganim <madhukar.pedagani@gmail.com>
2025-11-04 15:58:03 +1100

    chore: relax checkstyle to not fail build; Spotless applied; tests pinned to JDK21

--- src/main/java/com/myorg/myapp/repository/repository/UserRepository.java
+++ src/main/java/com/myorg/myapp/repository/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.myorg.myapp.repository;
 
 import com.myorg.myapp.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+  Optional<User> findByEmail(String email);
+
+  boolean existsByEmail(String email);
 }
